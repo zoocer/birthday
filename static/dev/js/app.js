15693631327e3e3b451329df5695b9c83b2acef2
yifei <flylee.bjfu@gmail.com>
2014-10-28 13:34:06 +0800

    完善动画逻辑； 添加第二页内容；

--- static/dev/js/app.js
+++ static/dev/js/app.js
@@ -18,5 +18,29 @@ checkStyle();
 // 初始化swiper
 var mainSwiper = $('#main-swiper-container').swiper({
 	mode: 'vertical',
-	loop: false
+	loop: false,
+	// 初始化
+	onFirstInit: function(swiper){
+		// 第一页添加动画样式
+		var node = $($('[data-role="page-wrap"]')[0]);
+		node.addClass('ani-box');
+	},
+	// 切换动画开始
+	onSlideChangeStart: function(swiper, direction){
+		// 在当前页，添加动画样式
+		var curIndex = mainSwiper.activeIndex;
+		var node = $($('[data-role="page-wrap"]')[curIndex]);
+		node.addClass('ani-box');
+	},
+	// 切换动画结束
+	onSlideChangeEnd: function(swiper, direction){
+		// 除当前页，其他页面去除动画样式
+		var curIndex = mainSwiper.activeIndex;
+		var node = $('[data-role="page-wrap"]');
+		for(var i=0;i<node.length;i++) {
+			if(i !== curIndex) {
+				$(node[i]).removeClass('ani-box');
+			}
+		}
+	}
 });
