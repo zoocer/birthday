d8691249a621d8e4f840e468e43a895b676b98ba
yifei <flylee.bjfu@gmail.com>
2014-10-28 11:31:34 +0800

    添加swiper类库； 第一页效果测试；

--- static/dev/js/app.js
+++ static/dev/js/app.js
@@ -2,3 +2,21 @@
  * @fileoverview 业务逻辑
  * @authors yifei (yifei@zoocer.com)
  */
+
+var elMainSwiperContainer = $('#main-swiper-container');
+
+// 检查初始化样式
+function checkStyle() {
+	var innerHeight = window.innerHeight;
+	elMainSwiperContainer.css({
+		height: innerHeight
+	});
+}
+
+checkStyle();
+
+// 初始化swiper
+var mainSwiper = $('#main-swiper-container').swiper({
+	mode: 'vertical',
+	loop: false
+});
